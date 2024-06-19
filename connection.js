0d176184b69780f9df3ff540d1fbe83bb8f62ecf
Prince Maphupha <princemaphupha@Princes-MacBook-Air.local>
2024-06-19 15:00:32 +0200

    fix: threads

--- connection.js
+++ connection.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose')
 
 const uri = process.env.DB
 
-const db = mongoose.connect(uri)
+//const db = mongoose.connect(uri)
 
 module.exports = db
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,27 @@
 'use strict';
-const Thread = require('../models/threads').Thread
+require('dotenv').config()
+const mongoose = require('mongoose')
 const Board = require('../models/board').Board
 const Reply = require('../models/reply').Reply
+
+const uri = process.env.DB
+
+mongoose.connect(uri)
+
+const Schema = mongoose.Schema;
+
+const threadSchema = new Schema({
+  _id        : Schema.Types.ObjectId,
+  text       : { type: String, required: true },
+  password   : { type: String, required: true },
+  created_on : { type: Date, default: Date.now },
+  bumped_on  : { type: Date, default: Date.now },
+  reported   : { type: Boolean, default: false },
+  replies    : [{ type: Schema.Types.ObjectId, ref: 'Reply' }]
+});
+
+const Thread = mongoose.model('Thread', threadSchema)
+
 
 module.exports = function (app) {
   
@@ -12,12 +32,18 @@ module.exports = function (app) {
       try {
         let newThread = new Thread({
           _id: new mongoose.Types.ObjectId(),
-          text: req.body.text,
-          password: req.body.delete_password,
+          text: text,
+          password: delete_password,
           created_on: new Date(),
           bumped_on: new Date(),
           reported: false
         });
+
+        newThread.save()
+          .then((thread) => {
+            console.log('Thread Added')
+          })
+        
       } catch (err){
         res.send('Error in creating record')
       }
